d5e5a716497f28ce0f318aead77b3fd1a9cc72ad
Adewale Orotayo <didymus7007@gmail.com>
2020-07-15 16:59:06 +0100

    error fix

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :event, aliases: [:hosted_event, :attended_event] do
+  factory :event, aliases: %i[hosted_event attended_event] do
     location { Faker::Address.building_number }
     date { Faker::Date.between(from: 1.year.ago, to: 1.year.from_now) }
     description { Faker::Lorem.sentence }
